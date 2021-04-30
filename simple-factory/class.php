e4333abcb6c1bf37a3d2cb51b6b748b16e9ed0fb
libin <bin.li@gaea.com>
2021-04-30 10:03:02 +0800

    factory

---
+++ simple-factory/class.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Class FactoryClass
+ *
+ * 简单工厂
+ *
+ * 场景：汽车表层刷漆工厂
+ */
+class FactoryClass
+{
+    /**
+     * 实现汽车上色
+     */
+    public static function production(string $color) {
+        switch ($color)
+        {
+            case 'violet':
+                return new VioletClass();
+                break;
+            case 'yellow':
+                return new YellowClass();
+                break;
+            case 'green':
+                return new GreenClass();
+                break;
+        }
+    }
+}
+
+/**
+ * Interface Product
+ *
+ * 定义接口类
+ */
+interface Product{
+    public function color_make() : String;
+}
+
+/**
+ * Class VioletClass
+ *
+ * 紫色车身涂改车间
+ */
+class VioletClass implements Product{
+    public function color_make(): String
+    {
+        return 'this car color is Violet';
+    }
+}
+
+/**
+ * Class VioletClass
+ *
+ * 紫色车身涂改车间
+ */
+class YellowClass implements Product{
+    public function color_make(): String
+    {
+        return 'this car color is Violet';
+    }
+}
+
+/**
+ * Class VioletClass
+ *
+ * 紫色车身涂改车间
+ */
+class GreenClass implements Product{
+    public function color_make(): String
+    {
+        return 'this car color is Violet';
+    }
+}
+
+// 客户端调用实现
+$factory = new FactoryClass();
+$class = $factory->production('violet');
+echo $class->color_make();
+
+// 输出
+// this car color is Violet
